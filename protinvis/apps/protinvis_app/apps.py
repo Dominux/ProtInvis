242c0d58d3c1095a46fa2f5c6db10e5054f2c91e
Hyper Elf <kir.gimranov@gmail.com>
2019-10-16 21:08:48 +0400

    Sooooo

--- protinvis/apps/protinvis_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProtinvisAppConfig(AppConfig):
-    name = 'protinvis_app'
--- protinvis/urls.py
+++ protinvis/urls.py
@@ -1,4 +1,4 @@
-"""protinvis URL Configuration
+"""ProtInvis URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('protinvis_app.urls')),
     path('admin/', admin.site.urls),
+    path('', include('protinvis_app.urls')),
 ]
