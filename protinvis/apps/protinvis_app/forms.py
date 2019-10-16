17713d9524352b6222f95bba5bd46b01edfd9e81
Hyper Elf <kir.gimranov@gmail.com>
2019-10-16 15:47:27 +0400

    Reload

---
+++ protinvis/apps/protinvis_app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import ProtInvis_Session
+
+from .constants import dyes_for_model
+
+class ProtInvis_SessionForm(forms.Form):
+    uniprot_id = forms.CharField(max_length=1000)
+    the_dye = forms.ChoiceField(choices=dyes_for_model)
+
+    def save(self):
+        new_session = ProtInvis_Session.objects.create(
+            uniprot_id=self.cleaned_data['uniprot_id'],
+            the_dye=self.cleaned_data['the_dye'],
+            )
+        return new_session
+        
--- protinvis/apps/protinvis_app/models.py
+++ protinvis/apps/protinvis_app/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from .constants import dyes_for_model
+
+class ProtInvis_Session(models.Model):
+    uniprot_id = models.CharField(max_length=1000)
+    the_dye = models.CharField(max_length=50, choices=dyes_for_model) 
+    use_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.uniprot_id, self.the_dye}"
