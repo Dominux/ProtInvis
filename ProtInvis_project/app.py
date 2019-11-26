73b7feb71f5ab9f6db56c5435bbd8f37e594b3d0
Dominux <kir.gimranov@gmail.com>
2019-11-26 23:13:14 +0400

    Added 'About' page

--- ProtInvis_project/app.py
+++ ProtInvis_project/app.py
@@ -23,5 +23,13 @@ def index():
 
     return render_template('index.html', form=form)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/index')
+def help():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
